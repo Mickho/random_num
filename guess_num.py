828ad1103148421df6aa6d6603e10de339617a1e
Mick Ho <mick.my.ho@gmail.com>
2019-01-17 22:04:22 +0800

    update new

--- guess_num.py
+++ guess_num.py
@@ -1,7 +1,10 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
+	count = count + 1 # count +=1
+	
 	num = input('please input number:')
 	num = int(num)
 	if num > r:
@@ -11,3 +14,4 @@ while True:
 	elif num == r:
 		print('Yes! You GOOD')
 		break
+	print('you guess', count, 'times')
